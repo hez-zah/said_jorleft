7987a0ddb91b68fd52ee86553de5834f046c7ba1
hez-zahi <124633794+hez-zah@users.noreply.github.com>
2024-11-26 23:42:03 +0100

    Add files via upload

---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sellyani <sellyani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 16:02:44 by sellyani          #+#    #+#             */
+/*   Updated: 2024/11/24 16:03:53 by sellyani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <limits.h>
+
+int	ft_printf(const char *format, ...);
+int	print_char(int c);
+int	print_digit(int nbr);
+int	print_hexa(unsigned int nbr, char specifier);
+int	print_ptr(unsigned long long nbr);
+int	print_str(char *str);
+int	print_unsigned(unsigned int nbr);
+
+#endif
